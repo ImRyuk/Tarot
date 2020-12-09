96ffc09d1af167adc493c0efa4c67c89850d9b3f
Clément <jacquelet.clement@outlook.fr>
2020-12-09 17:16:36 +0100

    Adding remove method for the ArrayList

--- src/cards/ArrayListCard.java
+++ src/cards/ArrayListCard.java
@@ -22,10 +22,30 @@ public class ArrayListCard {
 		return true;
 	}
 	
+	public boolean remove(int index) {
+		System.out.println(this.get(index));
+	    if (index < 0 || index >= size-1) {
+	        // FYI, this would be thrown anyway; not sure if you need to do it
+	        throw new IndexOutOfBoundsException("Index out of bounds.");
+	    }
+	    for(int i = 0; i < this.size; i++){
+	        if (i == index) {
+	            this.data[i] = null;
+	            //Then we modify each object's position from its actual position to the precedent one in order to get rid of the object we'll delete 
+	            for(int j = i; j < this.size; j++) {
+	            	this.data[j] = this.data[j+1];
+	            }
+	            //We decremente the size of the ArrayList
+	            size--;
+	        }
+	    }
+	    return true;
+	}
+	
 	public Object get(int index) 
 	{
 		if(index < 0 || index > size-1)
-			return null;
+			throw new IndexOutOfBoundsException("Index out of bounds.");
 		return data[index].toString();
 	}
 	
